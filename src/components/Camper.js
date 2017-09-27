dd69d13f77c0e99c457cd40960add85f335da97c
Simon Weller <s.meierbeck@gmail.com>
2017-09-27 13:56:36 +0200

    downloading sized avatar images

--- src/components/Camper.js
+++ src/components/Camper.js
@@ -8,7 +8,7 @@ const Camper = (props) => {
     <tr key={index}>
       <th>{index + 1}</th>
       <th>
-        <img src={camper.img} alt={camper.username} className="img-rounded camper-avatar"/>
+        <img src={camper.img + "&s=30"} alt={camper.username} className="img-rounded camper-avatar"/>
         <a href={"https://www.freecodecamp.org/" + camper.username} target="_blank">{camper.username}</a>
       </th>
       <th>{camper.recent}</th>
